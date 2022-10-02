fab4a3e03401be05191fb22bb90d719fc106cb3d
ItsZeroSliver <ga417@naver.com>
2022-10-02 17:49:04 +0900

    feat : Todo Entity 작성

--- src/main/java/todo/list/domain/Todo.java
+++ src/main/java/todo/list/domain/Todo.java
@@ -1,5 +1,34 @@
 package todo.list.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GeneratorType;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String name;
+    @Lob//긴글자
+    private String description;
+    private String status;
+    private String createdDate;
+
+
+    @PrePersist
+    public void CreatedDate(){
+        this.createdDate = LocalDateTime.now().toString();
+    }
 
 }
